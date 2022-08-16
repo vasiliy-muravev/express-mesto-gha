2b1ff3144f12d180324eb4f9f717bcb67318b63b
Vasily Muraviev <muravievvv@develop.mos.ru>
2022-08-16 05:22:52 +0300

    Mongoose schema models user and card

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+
+// подключаемся к серверу mongo
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+// подключаем мидлвары, роуты и всё остальное...
+
+app.listen(3000);
