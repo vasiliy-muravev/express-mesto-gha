d0d7f6d19c88e51c4e0bf11f7381380a714a8fee
v.muraviev <muravievVV@develop.mos.ru>
2022-09-05 22:40:14 +0300

    Tests errors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser');
 const { errors } = require('celebrate');
+const { ERROR_CODE_404 } = require('./constants/errorCode');
 
 const app = express();
 const usersRoutes = require('./routes/users');
@@ -9,7 +10,6 @@ const cardsRoutes = require('./routes/cards');
 const login = require('./routes/users');
 const createUser = require('./routes/users');
 const auth = require('./middlewares/auth');
-const NotFoundError = require('./errors/not-found-err');
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
@@ -28,8 +28,8 @@ app.use((err, req, res, next) => {
   res.status(err.statusCode).send({ message: err.message });
 });
 
-app.use((req, res, next) => {
-  next(new NotFoundError('Страница по указанному маршруту не найдена'));
+app.use((req, res) => {
+  res.status(ERROR_CODE_404).send({ message: 'Страница по указанному маршруту не найдена' });
 });
 
 app.listen(3000);
--- routes/users.js
+++ routes/users.js
@@ -41,7 +41,7 @@ usersRoutes.patch('/me/avatar', celebrate({
 /* Логин */
 usersRoutes.post('/signin', celebrate({
   body: Joi.object().keys({
-    email: Joi.string().required(),
+    email: Joi.string().required().email(),
     password: Joi.string().required(),
   }),
 }), login);
